05e2d053a4193def42f297f004ee5ade0c23c3d4
Rodrigo Mariano <rodspt@gmail.com>
2023-10-07 00:09:01 -0300

    Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,39 +2,11 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Database\Seeders\UserSeeder;
-
-class DatabaseSeeder extends Seeder
+class DatabaseSeeder extends PersonalSeeder
 {
     public function run(): void
     {
       $this->callUnique(UserSeeder::class);
     }
 
-    private function callUnique($class)
-    {
-        $nameClass = class_basename($class);
-        if (!$this->seederAlreadyExecuted($nameClass)) {
-            try{
-                $this->callOnce($class);
-                $this->markSeederAsExecuted($nameClass);
-            } catch (\Exception) {
-                throw new Exception("Ocorreu um erro na execução do seed: $nameClass");
-            }
-        }
-    }
-
-    private function seederAlreadyExecuted($seederName)
-    {
-        return \DB::table('seed')->where('name', $seederName)->exists();
-    }
-
-    private function markSeederAsExecuted($seederName)
-    {
-        \DB::table('seed')->insert([
-            'name' => $seederName,
-            'created_at' => now(),
-        ]);
-    }
 }
---
+++ database/seeders/PersonalSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class PersonalSeeder extends Seeder
+{
+
+    public function callUnique($class)
+    {
+        $nameClass = class_basename($class);
+        if (!$this->seederAlreadyExecuted($nameClass)) {
+            try{
+                $this->callOnce($class);
+                $this->markSeederAsExecuted($nameClass);
+            } catch (\Exception) {
+                throw new Exception("Ocorreu um erro na execução do seed: $nameClass");
+            }
+        }
+    }
+
+    private function seederAlreadyExecuted($seederName)
+    {
+        return \DB::table('seed')->where('name', $seederName)->exists();
+    }
+
+    private function markSeederAsExecuted($seederName)
+    {
+        \DB::table('seed')->insert([
+            'name' => $seederName,
+            'created_at' => now(),
+        ]);
+    }
+}
